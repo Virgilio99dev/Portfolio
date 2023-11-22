0841d3a8e1726ab153579f295ddf2dbdd94f281f
Virgilio99dev <133812169+Virgilio99dev@users.noreply.github.com>
2023-11-21 22:39:22 -0600

    Ver 1.0

---
+++ js/home.js
@@ -0,0 +1,22 @@
+const $ = document.querySelector.bind(document);
+$('#element').innerHTML = '';
+const typed = new Typed('#element', {
+    strings: [`
+    <section>
+        <p>
+            Hey there!! <br>
+            I am Virgilio, a highly motivated programmer with expertise in web development technologies and 3D modeling. I am eager to join the company's software development team.
+        </p>
+    </section>
+
+    <section>
+        <h2>Skills</h2>
+        <ul>
+            <li>HTML, Css, JavaScript,PHP and Python</li>
+            <li>Database Management (MySQL, MongoDB)</li>
+            <li>Version Control (Git, GitHub)</li>
+            <li>3D Modeling</li>
+        </ul>
+    </section>`],
+    typeSpeed: 10,
+  });
---
+++ js/index.js
@@ -0,0 +1,74 @@
+const $ = document.querySelector.bind(document);
+const $navlinks = document.querySelectorAll('.nav_link');
+
+function loadPage(page) {
+    if(page == 'home.html') {
+        fetch(`../html/${page}`)
+        .then(res => res.text())
+        .then(html => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, 'text/html');
+            let content = doc.querySelector('html');
+            var script = document.createElement('script'),
+                script2 = document.createElement('script'),
+                script3 = document.createElement('script');
+            script.src = 'https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js';
+            script.type = 'module';
+            script2.src = 'https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js';
+            script3.src = '../js/home.js';
+             $('main').innerHTML = '';
+             $('main').append(script, script2, script3, content);
+        })
+        .catch(err => {
+            let $err = document.createElement('h1').textContent = err;
+            $('main').appendChild($err);
+        });
+    } else {
+        fetch(`../html/${page}`)
+        .then(res => res.text())
+        .then(html => {
+            let parser = new DOMParser();
+            let doc = parser.parseFromString(html, 'text/html');
+            let content = doc.querySelector('html');
+            $('main').innerHTML = '';
+            $('main').appendChild(content);
+        })
+        .catch(err => {
+            let $err = document.createElement('h1').textContent = err;
+            $('main').appendChild($err);
+        });
+    
+    }
+} // loadPage
+
+document.addEventListener('click', (e) => {
+    if (e.target.matches('header a')) {
+        e.preventDefault();
+        loadPage(e.target.attributes.href.value);
+    }
+    if (e.target.matches('#icon-contact img')) {
+        $('footer').classList.add('active');
+    }
+    if (e.target.matches('#exit-icon')) {
+        $('footer').classList.remove('active');
+    }
+    if (e.target.matches('#icon-nav img')) {
+      $('.container_div_nav').classList.toggle('active');
+      $('#icon-nav-x').classList.toggle('active');
+      $('#icon-nav-menu').classList.toggle('active');
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadPage('home.html');
+    $('.nav_link').classList.add('active');
+});
+
+$navlinks.forEach(navlink => {
+    navlink.addEventListener('click', () => {
+        $navlinks.forEach(navlink => {
+            navlink.classList.remove('active');
+        });
+        navlink.classList.add('active');
+    });
+});
